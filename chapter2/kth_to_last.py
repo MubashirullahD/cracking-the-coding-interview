f6cdcedd8632ec9a9212649c9a4dd350c8df4277
Muhammad Mubashirullah Durrani <45071858+MubashirullahD@users.noreply.github.com>
2021-03-12 21:07:59 +0500

    add recursive implementation

--- chapter2/kth_to_last.py
+++ chapter2/kth_to_last.py
@@ -13,10 +13,9 @@ def kth_last(linkedlist, kth):
         count += 1
     # if there are 10 total elements and we get asked 3rd to last (k=3)
     # what do we do? 10-3 = 7 
-    # 10 last, 9 second, 8 third
+    # 10 last, 9 second, 8 third (add 1)
     want_node = count - kth + 1
-    print(want_node)
-    print(count)
+
     if want_node <= 0 or want_node > count:
         raise Exception("Number beyound size of linked list.", count)
 
@@ -24,11 +23,29 @@ def kth_last(linkedlist, kth):
     count = 1
     if count == want_node:
         print("Kth to last,", node.data)
+
     while(node.next is not None):
         node = node.next
         count += 1
         if count == want_node:
             print("Kth to last,", node.data)
+
+
+def kth_last_recursive(node, kth):
+    if node.next is None:
+        if kth == 1:
+            print("Kth to last", node.data)
+        return 1
+        
+    count = kth_last_recursive(node.next, kth) + 1
+    if count == kth:
+        print("Kth to last", node.data)
+    return count
+
+    # We should write another function where we call this one.
+    # This way we can give the head node reference and check if k is a valid input
+    # Interesting, invalid inputs dont return anything with this implementation
+
 
 if __name__ == "__main__":
     test_list = linkedlist(10)
@@ -37,4 +54,5 @@ if __name__ == "__main__":
     test_list.head.append_to_tail(40) 
     test_list.head.append_to_tail(50)
     kth_last(test_list, 5)
+    kth_last_recursive(test_list.head, 5)
     
